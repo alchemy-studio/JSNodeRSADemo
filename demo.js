795b9786e62118e0a5fd33a45c5e3bfe1c850f36
阿男 <l.weinan@gmail.com>
2020-01-18 10:07:05 +0800

    .

--- demo.js
+++ demo.js
@@ -2,53 +2,50 @@ nodeRSA = require("node-rsa");
 
 // 生成不需要密码的钥匙对
 // https://serverfault.com/questions/366372/is-it-possible-to-generate-rsa-key-without-pass-phrase
-// $ openssl req -new -nodes -x509 -subj "/C=CN/ST=Beijing/L=Beijing/O=Personal/CN=foo.io" -keyout foo.key -out foo.crt
-
+// $ openssl genrsa -out key.pem 2048
 // 得到私钥
-// $ head foo.key
-const key = new nodeRSA("-----BEGIN PRIVATE KEY-----\n" +
-    "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDOr9XjzgS4bEtY\n" +
-    "NVszJJjBsHkIIm6j+mcfrwvrkt19rHZ85eKtNnN0KKW7eDZ8S+h2FZAxEDYBYO5C\n" +
-    "j/hKqXiMLx7myQ6bR2aErISeAin9/Twar+0z6K4igVx1Tx3wYqGw1hA+mfeMqYf9\n" +
-    "m0SSfIcAEOs48zpZshucAj37Cb9KeDyaIdedZ0y2Z8+lzpzvkRIM5z46Dr5FMcP5\n" +
-    "L2K9Cg3GTVxMJkYCZokw7ZFim+fHhQ4kXEsMD3Tvre4HZ9PMJjht5793IVZX8+Rv\n" +
-    "ygl3151/+tp4YeMSLYjtBE/L6ZDu10q8hyPZHH4gpDOMheDo8uSx71mxL3Qur7XC\n" +
-    "oLiR0yyXAgMBAAECggEAFKw7rYRern+Oba0wiQYBDWZ0RJ8FB/u0stOUUqSm6LoG\n" +
-    "Q+nOaVrH43dpVD3q3TXOc3kmqfGgJTUnv5qc0ALxXrwb0COOBXP3yg9jWFaJErFw\n" +
-    "gI7WH9oHCMZ9k/Is4W1MFN7kUw6dVwFh67jK/AfejOAkVtblIDGTV2BWCLTp4C3G\n" +
-    "Y0tB2Scpas79gpdu7Mlhow4/SeTElYPvAKl7MVGp/UAmZ1LcAV+jWoSRUW8huqIG\n" +
-    "4Dei341My+5Bxz48dzwRRxZAXzZNPrw9ZiKnzdWAJBz5j8vF3tjnOVUJdKxV+ZPs\n" +
-    "lJ8dh6Ux2F5jgOrynguZDVfn5t8Rf7oeJzFSEUtUiQKBgQD4aUJowaLhlX3+Sctq\n" +
-    "H+gnpcIlBfXDTzCuD+IJRWuKl48QmqYw1Ry/d68ywu2JRWy0fWvyxaas2QdmX8qB\n" +
-    "bBiRov6Wo1uqGSJbeqNTr+x0/D1v/IL1gxcdemHItIly0sjrglNZair1KWtHMvqP\n" +
-    "LIa49AHg6jSGo5RV6/IY3JJsUwKBgQDVAEOj9MLzgcByb2K0bF9IMaUuwYKuZ1u1\n" +
-    "xppotMiZzFxIgu//xCEarUHQE3WJ/BIe57Wuk87RBdrps0CKXEiGcyKrGAM0TwHM\n" +
-    "+a3q/h0mB70JwXPGwQHz2tI4mpEG9C+P1BZqhpT/HyBm98YnKtH7Myt67xcITbCQ\n" +
-    "4OVUHT0WLQKBgQDttvTJAHwZOZBvVjjlXpEv16y490Ie29oux5471wW5g51xezyM\n" +
-    "QHQZObQbdHQRR7Fl6xvxH3mHsMwnCIXmYgYE4CCXEM54MUMynWJMhG5X6MX2M4My\n" +
-    "5HarS+DlSva5PcLttDKEv1dKco9pe/xQJIDr3v0pOZZ4PXOG4Zo0dmdcZwKBgQDP\n" +
-    "f6ZlPrf8SFaWJHRfl4Nq7RIh7lnEVOQ+vO8HObaCPVdZPFVZMFOfjVEjxxEmDu4I\n" +
-    "NWoEW/GKJs4QBJPHNgdhEyCe1JGqj50Ndd47orn6pmc97bs/0B561J0ej1ThFViQ\n" +
-    "IOiFdf4IWuZR6OacmVZeBIdDB0/EwwpS2QQPYTx1MQKBgQCNv2lM2LrmIRwntUPn\n" +
-    "bIPatQ0dSlqD05MPRqwZD2X1M6gkwpmFtx7ra+ZdYrH7ETOBnsTjkAlboMpUfjK6\n" +
-    "KoK/sce3MZV6TLEbYspbsiSKrfwNzRfqEnqcBViCkRBmNeOM1kb3J4RvPq5IilgG\n" +
-    "+HycLKU2irMKj+466Y593kWHNA==\n" +
-    "-----END PRIVATE KEY-----\n");
+const key = new nodeRSA("-----BEGIN RSA PRIVATE KEY-----\n" +
+    "MIIEogIBAAKCAQEAxSWnxllXurPO0YnD2dBWlPTzVhWAPJ1m/yloUyiDm3HUd5xr\n" +
+    "Um3Lb75B0OYfbUkHig4hsfva6AP1HlIMlwIXFLoRjR80C/fHySELG+uOBv+wbQh/\n" +
+    "l8UhL4NTM7IUmWCxKsaSWiA/39GH2MVSS8CAlY4eCVcAadfE9bXY/akEgEvwzJUo\n" +
+    "f1+HLFhQR9ECkLaKfsZMUY2CsZ+g4hRMqrf271Hk18ikMGjk2JP1KD4JibWep8ZE\n" +
+    "zpbN7AuztY9DE4BsV4S3Q2/ncvmUufHGd2QEQEUug22o04sU1WhLnl+bNDsLNzHU\n" +
+    "+dK31ddXYNKZpywauQcA1qaHAFVRGy8OlU9nswIDAQABAoIBACFVaZW++3dk+Z4y\n" +
+    "RfwpHsqW1CIFBqa5EnYoQSx2WDJLP9pthzPrvo8JQ0WX805eH7k3BHl8lRnPuX1D\n" +
+    "8D8EKnTltRwAX6N6ZLxcKu2wmVYTm3gEDNbTjPXkpYfDH3qDf9E0vAAjHcmxOt/r\n" +
+    "FSwbw2IGIov/uuEqi3HkKmlRT6KFaExlkCWF0Kgk2bEEFqCG5t1vdJzK6m1fTrtc\n" +
+    "APeb87+pgyygsncB/v5w7//EcklLpoZYJ3SIuzRuJwJ2KoyfosnOC10f4fM6DdjC\n" +
+    "/NJ7+beSvTvaiGmG1xsYIYpG3xeSdAzDfFXijqHp7732JlDYNRi1+GagIsAtibxp\n" +
+    "un+eIGkCgYEA9fM4kC4lCA0xynkSZQorHmS+Ao109oIv2Mvkr14jNVce0tdbi8BU\n" +
+    "lmD3kV3HkGUO0gyCALQAOUxWTU8+j1MiDHYIhC5X9L5Gm0w4TkJjJAx9fsOnkkoj\n" +
+    "qzQkVrbAiVn6z32veaOsveqOYudmCpAz3jtGpHfZ1uprmCM9vntb598CgYEAzTPt\n" +
+    "V+e+LWK2wY3U0z9969nwNtDtwalDSJdQBRXCrQrISzk9CjaitgDFzXgRj0314jUj\n" +
+    "MA7ZB5oQNMjAW1e1pYxPSL+tcX2Nl7V2QCYzBuLu9yVE2I0SSboMkKczqD2CnabO\n" +
+    "jAFRa5icVxoJMC8xNWx4F+Fi3oO8cYNm/+d7Cq0CgYBGbvCz2YMhWyX3JdTuuoq+\n" +
+    "mXsQoBHuxjmNjFZhIkRS8jARkTrWOq3+oqZDKugLoeUsoghYyzOFNSdB3H+968DE\n" +
+    "cMjfkaX+oL73mkxFnR1nJWyK8Nz8N98fGD+P1aYtr2b0JLory4Ocn59HE51VSRaR\n" +
+    "fcgtMWVg9qD5YJ49/nXDYQKBgDjzmYLVOa4FAK31fETpETy9LvM3x1UBckAaZsYS\n" +
+    "hQZbwL1H9I36HeyfiEZ/dXgtbfnLP6nd5RQ8CRVtRh3T2QFKUNLjB/NAomSho2Eo\n" +
+    "eO8HWsVTHalwdcvqjAQ1s5uU/sRcgGTRFNTk9TcjbbtmzaSDJ02Ra0xQiaODkbmi\n" +
+    "eD69AoGANXbTE+AUsZstQeLlGDP40Bn6lcxd4MjuxcMO9riLz9Dl1a2AqDdWpWFS\n" +
+    "NNqLtw2AfErjUXHsutXPTySGLN41QPFID3N3HEW4HxY5aKaecYeaJTFNzGYlE3As\n" +
+    "+0gaQ79zhrEJPZvOY/unSagkrLb8wU8pzD5MPk8dJD6utz7kQvk=\n" +
+    "-----END RSA PRIVATE KEY-----");
 
 // 查看钥匙对
 console.log(key.keyPair);
 console.log("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n");
 
 // 从私钥推导出公钥
-// $ openssl rsa -in foo.key -pubout
+// $ openssl rsa -in foo.key -pubout > foo.pub
 const pubKey = new nodeRSA("-----BEGIN PUBLIC KEY-----\n" +
-    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzq/V484EuGxLWDVbMySY\n" +
-    "wbB5CCJuo/pnH68L65Ldfax2fOXirTZzdCilu3g2fEvodhWQMRA2AWDuQo/4Sql4\n" +
-    "jC8e5skOm0dmhKyEngIp/f08Gq/tM+iuIoFcdU8d8GKhsNYQPpn3jKmH/ZtEknyH\n" +
-    "ABDrOPM6WbIbnAI9+wm/Sng8miHXnWdMtmfPpc6c75ESDOc+Og6+RTHD+S9ivQoN\n" +
-    "xk1cTCZGAmaJMO2RYpvnx4UOJFxLDA90763uB2fTzCY4bee/dyFWV/Pkb8oJd9ed\n" +
-    "f/raeGHjEi2I7QRPy+mQ7tdKvIcj2Rx+IKQzjIXg6PLkse9ZsS90Lq+1wqC4kdMs\n" +
-    "lwIDAQAB\n" +
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAxSWnxllXurPO0YnD2dBW\n" +
+    "lPTzVhWAPJ1m/yloUyiDm3HUd5xrUm3Lb75B0OYfbUkHig4hsfva6AP1HlIMlwIX\n" +
+    "FLoRjR80C/fHySELG+uOBv+wbQh/l8UhL4NTM7IUmWCxKsaSWiA/39GH2MVSS8CA\n" +
+    "lY4eCVcAadfE9bXY/akEgEvwzJUof1+HLFhQR9ECkLaKfsZMUY2CsZ+g4hRMqrf2\n" +
+    "71Hk18ikMGjk2JP1KD4JibWep8ZEzpbN7AuztY9DE4BsV4S3Q2/ncvmUufHGd2QE\n" +
+    "QEUug22o04sU1WhLnl+bNDsLNzHU+dK31ddXYNKZpywauQcA1qaHAFVRGy8OlU9n\n" +
+    "swIDAQAB\n" +
     "-----END PUBLIC KEY-----");
 
 // 公钥加密
